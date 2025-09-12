15c9f047780b8f3d224225139e8ab7054322c52d
TeeAaTeeUu <tatu.tallberg@gmail.com>
2025-09-12 23:05:57 +0200

    function-processor: avg and sort and reverse

--- test/function-processor.js
+++ test/function-processor.js
@@ -91,4 +91,31 @@ describe('function-processor', () => {
     output = -15
     assert.deepEqual(processFunction(result, pipe), output)
   })
+
+  it('avg', function () {
+    const result = [4, 2, 7, 3]
+    const output = 4
+
+    let pipe = parseFunction(this.test.title)
+    assert.deepEqual(processFunction(result, pipe), output)
+
+    pipe = parseFunction('average')
+    assert.deepEqual(processFunction(result, pipe), output)
+  })
+
+  it('sort', function () {
+    const result = [4, 2, 'bc', 7, 'ab', 3, true, false, [], [3], { a: 5 }, {}]
+    const output = [[], 2, 3, [3], 4, 7, { a: 5 }, {}, 'ab', 'bc', false, true]
+
+    const pipe = parseFunction(this.test.title)
+    assert.deepEqual(processFunction(result, pipe), output)
+  })
+
+  it('reverse', function () {
+    const result = [4, 2, 'bc', 7, 'ab', 3, true, false, [], [3], { a: 5 }, {}]
+    const output = [{}, { a: 5 }, [3], [], false, true, 3, 'ab', 7, 'bc', 2, 4]
+
+    const pipe = parseFunction(this.test.title)
+    assert.deepEqual(processFunction(result, pipe), output)
+  })
 })
